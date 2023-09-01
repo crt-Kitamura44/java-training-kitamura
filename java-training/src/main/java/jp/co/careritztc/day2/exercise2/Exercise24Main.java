1c4b0714d2a6649e9e771f82a14c52669d2e4ed4
Noriyuki Kitamura <noriyuki.kitamura@careritztc.co.jp>
2023-09-01 18:43:55 +0900

    2.4まで終了

--- java-training/src/main/java/jp/co/careritztc/day2/exercise2/Exercise24Main.java
+++ java-training/src/main/java/jp/co/careritztc/day2/exercise2/Exercise24Main.java
@@ -1,5 +1,11 @@
 package jp.co.careritztc.day2.exercise2;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+import jp.co.careritztc.day2.exercise2.data.ExampleTypeMap;
+
 /**
  * Javaの基礎５日間コース ２日目.
  *
@@ -22,5 +28,27 @@ public class Exercise24Main {
 
     // サンプルデータ５名全員を対象とした平均給与額を円およびドル換算し、それぞれ標準出力へ表示して下さい。
     // なお、ドル換算に使用するレートは下記の通りとし、BigDecimalを使用して計算して下さい。
+    BigDecimal averageIncomeDollar = BigDecimal.valueOf(0.00);
+    Integer averageIncomeYen = 0;
+    Integer totalIncomeYen = 0;
+
+    for(var entry: ExampleTypeMap.getSampleData().entrySet()){
+        totalIncomeYen = totalIncomeYen + entry.getValue().getSalary();
+    };
+
+    /* IntStream使用版
+    ArrayList<Integer> salaryList = new ArrayList<Integer>();
+    ExampleTypeMap.getSampleData().entrySet().forEach(entry -> {
+      salaryList.add(entry.getValue().getSalary());
+    });
+    averageIncomeYen = (int)salaryList.stream().mapToInt(Integer::intValue).average().getAsDouble();
+    */
+
+    averageIncomeYen = totalIncomeYen / ExampleTypeMap.getSampleData().entrySet().size();
+    averageIncomeDollar = BigDecimal.valueOf(averageIncomeYen).divide(BigDecimal.valueOf(FIX_RATE_YEN_DOLLAR),3, RoundingMode.DOWN);
+
+    System.out.println(String.format("平均給与は%d円です。",averageIncomeYen));
+    System.out.println(String.format("平均給与は%.2fドルです。",averageIncomeDollar));
+
   }
 }
